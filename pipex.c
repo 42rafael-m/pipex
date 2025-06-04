39eb46725b4d63968f78c5fd0d5e46a796671ddb
RafaelMS <martinsoeder35@gmail.com>
2025-06-04 11:54:20 +0200

    a comprobar

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: rafael-m <rafael-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 15:48:27 by rafael-m          #+#    #+#             */
-/*   Updated: 2025/06/03 19:39:49 by rafael-m         ###   ########.fr       */
+/*   Updated: 2025/06/03 19:49:11 by rafael-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ int	ft_parent(char *outfile, int pipefd, char *cmd, char *p, char **envp)
 	close(outfd);
 	argv = ft_split(cmd, ' ');
 	if (!argv)
-		return (free(cmd_p), perror("malloc"), errno);
-	if (execve(cmd_p, argv, env) == -1)
-		return (ft_free_d(argv), free(cmd_p), perror("execve"), errno);
+		return (free(p), perror("malloc"), errno);
+	if (execve(p, argv, envp) == -1)
+		return (ft_free_d(argv), free(p), perror("execve"), errno);
 	return (errno);
 }
 int	ft_child(char *infile, int pipefd, char *cmd, char *cmd_p, char **env)
@@ -61,7 +61,6 @@ int	ft_pipe_fork(char *infile, char *outfile, char *p1, char *p2, char *cmd1, ch
 	int	pipefd[2];
 	int	pid;
 	int	status;
-	int	child;
 
 	if (pipe(pipefd) == -1)
 		return (free(p1), free(p2), perror("pipe"), exit(errno), 1);
@@ -72,22 +71,22 @@ int	ft_pipe_fork(char *infile, char *outfile, char *p1, char *p2, char *cmd1, ch
 		ft_child(infile, pipefd[1], cmd1, p1, envp);
 	if (waitpid(pid, &status, 0) == -1)
 		return (free(p1), free(p2), perror("wait"), exit(errno), 1);
-	if (!ft_parent(outfile, pipefd[0], cmd2, p2, envp))
-	if (dup2(pipefd[1], STDOUT_FILENO) == -1)
-		return (free(p1), free(p2), perror("dup2"), exit(errno), 1);
-	infile = open(infile, )
+	if (ft_parent(outfile, pipefd[0], cmd2, p2, envp))
+		return (errno);
+	return (errno);
 }
 
 char	*ft_cmd_s(char *cmd)
 {
 	char    *pos;
+	char	*cmd_s;
 	char	*r;
 
 	pos = ft_strchr(cmd, ' ');
 	if (pos)
- 		cmd_s = ft_substr(0, cmd - pos);
+ 		cmd_s = ft_substr(cmd, 0, cmd - pos);
 	else
-		cmd_s = ft_substr(0, ft_strlen(cmd));
+		cmd_s = ft_substr(cmd, 0, ft_strlen(cmd));
  	if (!cmd_s)
 		return (perror("malloc"), NULL);
 	if (ft_strchr(cmd_s, '/'))
@@ -118,7 +117,7 @@ char	*ft_parse_cmd(char *cmd, char **env)
 	{
 		if (ft_strstr(env[i], "PATH") && !ft_strstr(env[i], "_PATH"))
 		{
-			cmd_path = ft_strjoin(env[i] + 5, file);
+			cmd_path = ft_strjoin(env[i] + 5, cmd);
 			if (!cmd_path)
 				return (perror("malloc"), NULL);
 			break ;
@@ -133,7 +132,7 @@ char	*ft_parse_cmd(char *cmd, char **env)
 int	ft_parse_file(char *file, char **env, int mode)
 {
 	int	i;
-	int	aacc;
+	int	acc;
 	char	*t;
 	char	*file_path;
 
@@ -142,7 +141,7 @@ int	ft_parse_file(char *file, char **env, int mode)
 		return (access(file, mode));
 	while (env[i])
 	{
-		if ((ft_strstr(env[i]), "PWD") && !ft_strstr(env[i], "OLDPWD"))
+		if (ft_strstr(env[i], "PWD") && !ft_strstr(env[i], "OLDPWD"))
 		{
 			t = ft_strjoin(env[i] + 4, "/");
 			file_path = ft_strjoin(t, file);
@@ -157,20 +156,19 @@ int	ft_parse_file(char *file, char **env, int mode)
 
 int	main(int argc, char **argv, char **envp)
 {
-	int	status;
 	char	*p1;
-	char    *p1;
+	char    *p2;
 	int	status;
 
 	status = 0;
 	if (argc != 5)
 		return (write(2, "file1 cmd1 cmd2 file2", 21), 1);
-	if (ft_parse_file(argv[1], R_OK) == -1)
+	if (ft_parse_file(argv[1], envp, R_OK) == -1)
 		return(perror("access"), errno);
-	if (ft_parse_file(argv[4], W_OK) == -1)
+	if (ft_parse_file(argv[4], envp, W_OK) == -1)
 		status++;
 	p1 = ft_parse_cmd(argv[2], envp);
 	p2 = ft_parse_cmd(argv[3], envp);
-	status = ft_pipe_fork(infile, outfile, p1, p2, argv[2], argv[3], envp);
-	return (free(cmd1_path), free(cmd2_path), status);
+	status = ft_pipe_fork(argv[1], argv[4], p1, p2, argv[2], argv[3], envp);
+	return (free(p1), free(p2), status);
 }
